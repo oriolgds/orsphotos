25d33b5c03b5ca1700caef751f923531b85020fa
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-01-01 13:40:57 +0100

    Adding margins to cards at home

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -4,35 +4,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Expanded(
-          child: Card(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  Text('Tus fotos', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.start,)
-                ],
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Card(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: const Column(
-                children: [
-                  Text('Tus fotos')
-                ],
-              ),
-            ),
-          ),
-        )
+        HomeCard(), HomeCard()
       ],
     );
   }
 }
+class HomeCard extends StatelessWidget {
+  const HomeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Tus fotos', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.start,)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
