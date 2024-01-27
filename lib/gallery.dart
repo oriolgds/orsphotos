29aaf366920439d28cb65699f8b14614d1cdd41a
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-01-27 20:06:48 +0100

    Build in linux

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,10 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 
 
-class GalleryPage extends StatelessWidget {
+class GalleryPage extends StatefulWidget {
   const GalleryPage({super.key});
 
+  @override
+  State<GalleryPage> createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+  directory() async {
+    final dir = Directory('/sdcard/DCIM/Camera/');
+    final List<FileSystemEntity> entities = await dir.list().toList();
+    debugPrint("All files: -------------------------------------------");
+    for (var element in entities) {
+      debugPrint(element.toString());
+    }
+  }
+  @override
+  void initState() {
+    directory();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
